7c064913c6d12a0a95180fea0fc7bfcb188e5103
Pekka Enberg <penberg@iki.fi>
2017-06-09 17:42:34 +0300

    Add application version

--- cmd/ustat/main.go
+++ cmd/ustat/main.go
@@ -12,11 +12,14 @@ import (
 	"time"
 )
 
+const version = "0.1.0"
+
 const defaultDelay = 1
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "ustat"
+	app.Version = version
 	app.Usage = "Unified system statistics collector"
 	app.Authors = []cli.Author{
 		cli.Author{
